bc7ba73be3c9f7379291d9ba58e39d56e5d73a0d
raduainrimon <163767983+raduainrimon@users.noreply.github.com>
2024-10-06 20:41:29 +0600

    html css for default page..
    he he he he

--- food-del/frontend/src/components/Navbar/Navbar.jsx
+++ food-del/frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 
 const Navbar = () => {
+  const [menu, setMenu] =
+    useState("home"); /*default underline state for navbar*/
+
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" classname="logo" />
+      <img src={assets.logo} alt="Website Logo" classname="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact us</li>
+        <li className={menu === "home" ? "active" : ""}>home</li>
+        <li className={menu === "menu" ? "active" : ""}>menu</li>
+        <li className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
+        <li className={menu === "contact-us" ? "active" : ""}>contact us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
@@ -18,7 +21,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>sign in</button>
+        <button>Sign In</button>
       </div>
     </div>
   );
