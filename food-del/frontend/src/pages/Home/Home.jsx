7814b38b550bbb0fe85353d5252a0ff7109f36af
raduainrimon <163767983+raduainrimon@users.noreply.github.com>
2024-10-08 23:45:01 +0600

    hrhrh
    hrhrh

--- food-del/frontend/src/pages/Home/Home.jsx
+++ food-del/frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Home.css";
+import Header from "../../components/Header/Header";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
