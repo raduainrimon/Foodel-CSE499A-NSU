b1e74601f44398946b2631a16cedf8fba494caaa
raduainrimon <163767983+raduainrimon@users.noreply.github.com>
2024-10-11 13:25:37 +0600

    completion of footer
    he he he

---
+++ food-del/frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+8801756086896</li>
+            <li>raduain.rimon@northsouth.edu</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Foodel.com - All Right Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
