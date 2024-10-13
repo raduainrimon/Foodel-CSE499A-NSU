98c367fe888683657c3248a9d69a56a889474002
raduainrimon <163767983+raduainrimon@users.noreply.github.com>
2024-10-13 19:32:56 +0600

    sign up
    he he he

---
+++ food-del/frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./LoginPopup.css";
+
+const LoginPopup = () => {
+  const [currState, setCurrState] = useState("Sign Up");
+
+  return (
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPopup;
