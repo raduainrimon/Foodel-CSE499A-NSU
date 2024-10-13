5ae74f2242dce679235a62c3662b908fbd5c47b4
raduainrimon <163767983+raduainrimon@users.noreply.github.com>
2024-10-14 01:09:56 +0600

    login sign in sign up
    he he he

--- food-del/frontend/src/App.jsx
+++ food-del/frontend/src/App.jsx
@@ -9,9 +9,10 @@ import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
-      {showLogin ? <LoginPopup /> : <></>}
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
