e92ab88990b6ff21a711c97d3795c84e983846b6
raduainrimon <163767983+raduainrimon@users.noreply.github.com>
2024-10-14 12:01:31 +0600

    creating cart page and completion
    he he he he

--- food-del/frontend/src/components/Navbar/Navbar.jsx
+++ food-del/frontend/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 
 const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] =
     useState("home"); /*default underline state for navbar*/
 
+  const { getTotalCartAmount } = useContext(StoreContext);
+
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="Website Logo" className="logo" />
+      <Link to="/">
+        <img src={assets.logo} alt="Website Logo" className="logo" />
+      </Link>
       <ul className="navbar-menu">
         <Link
           to="/"
@@ -44,8 +49,10 @@ const Navbar = ({ setShowLogin }) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
         <button onClick={() => setShowLogin(true)}>Sign In</button>
       </div>
